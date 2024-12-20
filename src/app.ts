2c616bbdddd7a3f4a51b76f5813aba8d03bcbaa8
Asad <fensami2323@gmail.com>
2024-12-20 09:22:56 +0600

    Blog Assignment Project is open

---
+++ src/app.ts
@@ -0,0 +1,24 @@
+import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
+// import globalErrorHandler from './app/middlewares/globalErrorHandler';
+const app: Application = express();
+
+// parsers
+app.use(express.json());
+app.use(cors());
+
+// application routers
+// app.use('/api', router);
+
+const test = async (req: Request, res: Response) => {
+    // Promise.reject()
+    // const a = 10;
+    res.send("server run succesfully , Welcome to Blog Project");
+};
+
+app.get('/', test);
+// app.use(globalErrorHandler)
+
+
+
+export default app;
---
+++ src/server.ts
@@ -0,0 +1,37 @@
+import app from './app';
+import mongoose from 'mongoose';
+import config from './app/config';
+import { Server } from 'http';
+// const mongoose = require('mongoose');
+let server: Server;
+
+async function main() {
+    try {
+        await mongoose.connect(config.database_url as string);
+
+        server = app.listen(config.port, () => {
+            console.log(`Example app listening on port ${config.port}`);
+        });
+    } catch (err) {
+        console.log(err);
+    }
+
+}
+
+main();
+
+process.on('unhandledRejection', () => {
+    console.log('😤 unhandledRejection is detected, shutting down ...');
+
+    if (server) {
+        server.close(() => {
+            process.exit(1)
+        })
+    }
+})
+
+
+process.on('uncaughtException', () => {
+    console.log('😤 uncaughtException is detected, shutting down ...');
+    process.exit(1)
+})
